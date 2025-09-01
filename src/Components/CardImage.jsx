68cbcf03b145585afb492f64069b117aa07c07b0
Felix Kurniawan Dharma <felix.dharma121299@gmail.com>
2025-09-01 19:03:07 +0700

    Edited Chapter 3

--- src/Components/CardImage.jsx
+++ src/Components/CardImage.jsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -6,32 +7,82 @@ import "../styles/common.css";
 
 function CardImage(props) {
   return (
-    <div className="card-container">
-      <div className="card-content-custom">
-        <CardMedia
-          sx={{ height: 140 }}
-          image="/arrowup.png"
-          title="green iguana"
-        />
-        <CardContent>
-          <div className="card-title-custom">
-            <Typography
-              sx={{ textAlign: "center" }}
-              variant="h2"
-              component="div"
-            >
-              {props.title}
-            </Typography>
-            <Typography variant="subtitle1">{props.subtitle}</Typography>
-            <ul>
+    <Card
+      sx={{
+        backgroundColor: "transparent",
+        borderRadius: 0,
+        boxShadow: "none",
+      }}
+    >
+      <React.Fragment>
+        <CardContent
+          sx={{
+            maxWidth: '300px',
+            minHeight: '300px',
+            height: 'auto',
+            backgroundColor: "#577BC1",
+            border: "15px solid #344CB7",
+            borderRadius: "25px",
+          }}
+        >
+
+          <CardMedia
+            sx={{
+              height: props.images.height,
+              width: props.images.width,
+              objectFit: 'cover',
+              objectPosition: 'center',
+              mx: 'auto',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+            }}
+            component="img"
+            src={props.images.src}
+            alt="arrowhead">
+
+          </CardMedia>
+
+
+
+          <Typography
+            sx={{
+              textAlign: "center",
+              fontSize: "20px",
+              fontWeight: "bold",
+              textShadow: "1px 1px 2px rgba(104, 104, 104, 0.3)",
+              fontFamily: "'Press Start 2P',  system-ui",
+              lineHeight: "3vh",
+              marginTop: "2vh",
+              color: "#ffeb00",
+            }}
+            variant="h2"
+            component="div"
+          >
+            {props.title}
+          </Typography>
+
+          <Typography
+            sx={{
+              padding: "10px",
+              textAlign: "center",
+              fontFamily: "'Press Start 2P',  system-ui",
+              fontSize: "10px",
+              color: "#ffeb00",
+            }}
+            variant="subtitle1"
+          >
+            {props.subtitle}
+          </Typography>
+
+          <ul>
+            <div className="custom-list">
               {props.items.map((item, index) => (
-                <li key={index}> {item} </li>
+                <li key={index}>{item}</li>
               ))}
-            </ul>
-          </div>
+            </div>
+          </ul>
         </CardContent>
-      </div>
-    </div>
+      </React.Fragment>
+    </Card>
   );
 }
 
--- src/Components/Chapter3.jsx
+++ src/Components/Chapter3.jsx
@@ -2,38 +2,59 @@ import "../styles/common.css";
 import "../styles/chapter3.css";
 import Stars from "./Stars.jsx";
 import CardImage from "./CardImage.jsx";
-import { Card } from "@mui/material";
 
 function Chapter3(props) {
-  return (
-    <section
-      id="chapter3"
-      style={{
-        position: "relative",
-        minHeight: "100vh",
-        overflow: "hidden",
-        width: "100%",
-      }}
-    >
-      <Stars />
-      <div className="title-container">
-        <h1> {props.title} </h1>
-        <div className="subtitle-container">
-          <h2> {props.subtitle} </h2>
-        </div>
-      </div>
+    return (
+        <section
+            id="chapter3"
+            style={{
+                position: "relative",
+                minHeight: "100vh",
+                overflow: "hidden",
+                width: "100%",
+            }}
+        >
+            <Stars />
+            <div className="title-container">
+                <h1> {props.title} </h1>
+                <div className="subtitle-container">
+                    <h2> {props.subtitle} </h2>
+                </div>
+            </div>
+            <div className="card-container">
 
-      <CardImage
-        title="PT Realta Chakra Dharma"
-        subtitle="Working as Fullstack .NET Developer"
-        items={[
-          "Designing, Creating and Bug Fixing programs for Hospitality and Golf Management.",
-          "Creating and Bug Fixing an API for Caddy Apps.",
-          "Designing, Creating and Bug Fixing Reports using Crystal Reports.",
-        ]}
-      />
-    </section>
-  );
+                <CardImage
+                    title="PT Realta Chakra Dharma"
+                    subtitle="Working as Fullstack .NET Developer - 2 Years++ WOE"
+                    items={[
+                        "Designing, Creating and Bug Fixing programs for Hospitality and Golf Management.",
+                        "Creating and Bug Fixing an API for Caddy Apps.",
+                        "Designing, Creating and Bug Fixing Reports using Crystal Reports.",
+                    ]}
+                    images={{
+                        height: 100,
+                        width: 100,
+                        src: "/logo-realta.jpg"
+                    }}
+                />
+
+                <CardImage
+                    title="Udemy Course"
+                    subtitle="Fullstack React Web Developer"
+                    items={[
+                        "Learning From Backend to Frontend",
+                        "Learning and Practicing make REST API using NodeJs and PostgreSQL",
+                        "Designing, Creating and Bug Fixing Reports using Crystal Reports.",
+                    ]}
+                    images={{
+                        height: 100,
+                        width: 200,
+                        src: "/logo-udemy.png"
+                    }}
+                />
+            </div>
+        </section>
+    );
 }
 
 export default Chapter3;
